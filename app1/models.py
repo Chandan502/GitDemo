741b7c09e963c1623ac5f621a5bb4cc0ae6560f5
chandan <maitychandan1502@gmail.com>
2021-09-28 19:20:50 +0530

    The Model is created

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	pno = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30)
+	price = models.IntegerField()
